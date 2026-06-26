d88f3fb36b3d75839ec611426edf4afb50af9747
derek3522-ops <derek3522@gmail.com>
2026-06-26 11:09:22 -0700

    Update products.js

--- app/lib/products.js
+++ app/lib/products.js
@@ -360,7 +360,7 @@ export const products = [
     "category": "Keypads",
     "cost": 437.00,
     "price": 550.00,
-    "description": "EPP, PCI V5.X CERTIFIED, EPP B5",
+    "description": "EPP, PCI V5.X CERTIFIED, EPP B5. NOTICE: This Keypad Requires ATM Software Version V05.00.37 Or Higher",
     "partNumber": "201207541",
     "image": "/products/201207541.png",
     "inStock": true
