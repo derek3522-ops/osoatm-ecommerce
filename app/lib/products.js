81a63748389d30d4be4d1ddc6c61e048bcffe8b2
derek3522-ops <derek3522@gmail.com>
2026-06-29 11:55:02 -0700

    Update products.js

--- app/lib/products.js
+++ app/lib/products.js
@@ -242,6 +242,7 @@ export const products = [
   { "label": "Verizon", "monthlyFee": 7 },
   { "label": "Dual (AT&T/Verizon)", "monthlyFee": 12 }
 ],
+    "image": "/products/placeholder.png",
     "inStock": true
   },
   {
